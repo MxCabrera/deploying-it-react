c19aaeab1efa57e647453e8f38fae48e6c995dfa
MxCabrera <53961785+MxCabrera@users.noreply.github.com>
2020-04-28 10:56:09 -0400

    Updates

---
+++ precache-manifest.c1f3d62c54ce7a23805416df2312d762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e174730cd713de06c396b30be530eb5",
+    "url": "/deploying-it-react/index.html"
+  },
+  {
+    "revision": "feae43dcc18ef5090f68",
+    "url": "/deploying-it-react/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "4a47023c2929a439a63f",
+    "url": "/deploying-it-react/static/js/2.31311555.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploying-it-react/static/js/2.31311555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feae43dcc18ef5090f68",
+    "url": "/deploying-it-react/static/js/main.43a686ac.chunk.js"
+  },
+  {
+    "revision": "0d6dc80ed6efeaf8b515",
+    "url": "/deploying-it-react/static/js/runtime-main.33935bff.js"
+  }
+]);
